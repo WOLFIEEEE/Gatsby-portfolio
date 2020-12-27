3aa5417321f77eda3363485d79f2bd46e65f5e70
KHUSHWANT PARIHAR <50989808+WOLFIEEEE@users.noreply.github.com>
2020-12-27 13:15:49 +0530

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  siteMetadata: {
+    title: "Wolfie Portfolio",
+    description: "This is My Portfolio website using gatsby and strapi and for recieving contact information I used formspree",
+    author: "@wolfie",
+    twitterUsername: "@khushwantparihar",
+    image: "/rrrrrr.png",
+    siteUrl: "https://wolfie-portfolio.netlify.app/",
+  },
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        //   contentTypes : `jobs`, `projects`, `blogs`,
+        //   singleType : `about`
+        //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
+        contentTypes: [`projects`,`checks`],
+        singleTypes: [`about`],
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-prefetch-google-fonts`,
+    //   options: {
+    //     fonts: [
+    //       {
+    //         family: `Roboto`,
+    //         variants: [`400`, `700`],
+    //       },
+    //       {
+    //         family: `Open Sans`,
+    //       },
+    //     ],
+    //   },
+    // },
+  ],
+}
