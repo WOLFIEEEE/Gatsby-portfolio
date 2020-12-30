6e5d6d1d028872f37770eb13caa655e86b875b35
KHUSHWANT PARIHAR <kgpkhushwant1@gmail.com>
2020-12-30 17:03:43 +0530

    troubleshooting

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,12 @@
 import React from "react"
-import Layout from "../components/Layout"
-import { Link } from "gatsby"
-import SEO from "../components/SEO"
+import back from "./background"
+
 
 const Error = () => {
 
-  return<Layout>
-    <SEO title="Error Page" />
-  <main className="error-page">
-    <div className="error-container">
-      <h1>oops it's a dead end</h1>
-      <Link to="/" className="btn">
-        Back Home
-      </Link>
-    </div>
-  </main>
-  </Layout> }
+  return(
+  <h1>error</h1>
+  );
+  }
 
 export default Error
---
+++ src/pages/background.js
@@ -0,0 +1,65 @@
+import React from "react"
+import "../css/background.css"
+import { Link } from "gatsby"
+import "../css/contact.css"
+import build from "../assets/build.gif"
+const back = () => {
+  return (
+    <div>
+
+    <div className="background">
+        <div className="container">
+          <div className="screen">
+            <div className="screen-header">
+              <div className="screen-header-left">
+                <div className="screen-header-button close" />
+                <div className="screen-header-button maximize" />
+                <div className="screen-header-button minimize" />
+              </div>
+              <div className="screen-header-right">
+                <div className="screen-header-ellipsis" />
+                <div className="screen-header-ellipsis" />
+                <div className="screen-header-ellipsis" />
+              </div>
+            </div>
+            <div className="screen-body">
+              <div className="screen-body-item left">
+                <div className="app-title">
+                  <span>CONTACT</span>
+                  <span>ME</span>
+                </div>
+                <div className="app-contact">CONTACT INFO : +91 8290938303</div>
+              </div>
+              <div className="screen-body-item">
+                <form action="https://formspree.io/f/xayllodz" method="POST">
+                  <div className="app-form">
+                    <div className="app-form-group">
+                      <input type="text" name="name" className="app-form-control" placeholder="NAME" />
+                    </div>
+                    <div className="app-form-group">
+                      <input type="email" placeholder="email" className="app-form-control" />
+                    </div>
+                    <div className="app-form-group message">
+                      <input name="message" className="app-form-control" placeholder="MESSAGE" />
+                    </div>
+                    <div className="app-form-group buttons">
+                      <button className="app-form-button">SEND</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h3 className="texta">For any suggestion for my portfolio Website Click below</h3>
+      <div className="textab">
+      <Link to="/build" className="btn" >
+       Add suggestion
+      </Link>
+      </div>
+  </div>
+  )
+}
+
+export default back
