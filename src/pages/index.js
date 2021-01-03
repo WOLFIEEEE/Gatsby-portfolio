676073a0d92c25452cd36ba89ecdb840d49ff4b4
KHUSHWANT PARIHAR <kgpkhushwant1@gmail.com>
2021-01-03 18:18:44 +0530

    Creating a welcome page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,42 @@ import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import Projects from "../components/Projects"
 import SEO from "../components/SEO"
+import Welcome from 'react-welcome-page'
 export default ({data}) => {
   const {allStrapiProjects:{nodes:projects}}=data
-  
-  return <Layout>
+  return (
+    <div>
+    <Welcome
+            loopDuration={1000} 
+            data={
+                [
+  {
+    "backgroundColor": "rgb(14, 14, 14)",
+    "textColor": "#B2B2B2",
+    "text": "cdscsdc",
+    "image": "https://guides.github.com/features/issues/labels.png" 
+  },
+  {
+    "backgroundColor": "rgb(24, 14, 12)",
+    "textColor": "#EEE6DF",
+    "text": "csdcds",
+    "image": "https://guides.github.com/features/issues/labels.png"
+  }
+]
+            }
+        />
+    
+
+  <Layout>
     <SEO title="Home" description="this is our home page" />
     
     <Hero/>
     <Projects  projects={projects} title="Main Projects" showLink/>
   </Layout>
-}
+  </div>
+
+);
+  }
 export const query = graphql`
   {
     allStrapiProjects(filter: {Featured: {eq: true}}) {
